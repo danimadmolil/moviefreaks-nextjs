73f0307c87a5633e9d90892e512bbac79c77864e
daniyal <dani.madmolil@gmail.com>
2022-10-26 03:43:21 +0330

    add github authentication

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../db/index";
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+});
